9ccbad319a50a51a5d89b858b44b118f0b339ac6
Tyler Sinks <tyler.sinks@gmail.com>
2021-04-28 15:56:09 -0400

    add ClearAll for test disposal

--- Pierre/Models/Bread.cs
+++ Pierre/Models/Bread.cs
@@ -7,18 +7,30 @@ namespace Pierre.Models
   {
     public int LoavesInOrder { get; set; }
     public int CostPerLoaf = 5;
+    private static List<Bread> _allBreads = new List<Bread> {};
     
     public Bread(int loavesInOrder)
     {
       LoavesInOrder = loavesInOrder;
+      _allBreads.Add(this);
     }
 
     public static int PriceOfLoaves()
     {
       int SubTotal = 0;
-      // GetAllBreads() gathers all instances of Bread
+      int allLoavesCount = 0;
+      foreach (Bread order in _allBreads)
+      {
+        allLoavesCount += order.LoavesInOrder;
+        SubTotal += order.CostPerLoaf * order.LoavesInOrder;
+      }
+      // if (allLoavesCount >= 2) buy 2, get 1 free
+      // {
+      //   apply discount
+      // }
 
       // iterate through all Breads, add calculated price of breads to SubTotal each time through
+
 
       // return SubTotal after loop
       return SubTotal;
@@ -30,6 +42,11 @@ namespace Pierre.Models
       List<Bread> allBreads = new List<Bread>{};
       
       return allBreads;
+    }
+
+    public static void ClearAll()
+    {
+      _allBreads.Clear();
     }
   }
 }
