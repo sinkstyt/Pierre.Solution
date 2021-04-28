0d474c728ab4dd25448f780419e012b8ba2d3c8e
Tyler Sinks <tyler.sinks@gmail.com>
2021-04-28 16:02:53 -0400

    fail well price of 5 loaves spec

--- Pierre.Tests/ModelTests/PierreTests.cs
+++ Pierre.Tests/ModelTests/PierreTests.cs
@@ -37,17 +37,18 @@ namespace Pierre.Tests
       int expected = 5;
       Assert.AreEqual(expected, price);
     }
+
+    [TestMethod]
+    public void PriceOfBreads_ReturnsAccuratePriceWhenLoavesInOrderIs5_20()
+    {
+      int orderSize = 5;
+      Bread forHungryPerson = new Bread(orderSize);
+      int expectedPrice = 20;
+      int received = Bread.PriceOfLoaves();
+      Assert.AreEqual(expectedPrice, received);
+    }
+
     // Matcher for LISTS in MS Test:
     // CollectionAssert.AreEqual(newList, result);
-
-    // [TestMethod]
-    // public void PriceOfBreads_ReturnsIntegerMeaningPriceWhileLoavesInOrderIs1_5()
-    // {
-    //   int orderSize = 1;
-    //   Bread SmallOrder = new Bread(orderSize);
-    //   int expectedPrice = 5;
-    //   int received = SmallOrder.PriceOfLoaves();
-    //   Assert.AreEqual(expectedPrice, received);
-    // }
   }
 }
